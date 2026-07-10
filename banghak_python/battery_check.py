a41e6c718b602c33487186704f36a92de472deaf
Rhw0213 <rhw0213@gmail.com>
2026-07-10 09:23:50 +0900

    log

---
+++ banghak_python/battery_check.py
@@ -0,0 +1,16 @@
+"""
+battery_check.py
+목적: 배터리 전압 확인 및 충전 상태 판단
+"""
+
+from robot_hat import utils
+
+voltage = utils.get_battery_voltage()
+print(f"배터리 전압: {voltage:.2f} V")
+
+if voltage > 7.8:
+    print("상태: 충분함")
+elif voltage >= 6.7:
+    print("상태: 보통")
+else:
+    print("상태: 낮음 - 충전 필요")
--- dcmotor.h
+++ dcmotor.h
@@ -22,4 +22,5 @@ void motor_set_speed(int speed)
 void motor_set_direction(int forward)
 void motor_stop(void)
 int lidar_read_distance_median(int sample)
+
 #endif
